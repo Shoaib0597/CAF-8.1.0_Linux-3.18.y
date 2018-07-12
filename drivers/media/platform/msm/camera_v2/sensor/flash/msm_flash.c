40e6d70aeac41e3f9f73023076064278d88205be
Louis Popi <theh2o64@gmail.com>
2018-07-12 12:53:26 +0200

    leds: Import Tinno's flash changes

--- drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
+++ drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
@@ -586,6 +586,10 @@ static int32_t msm_flash_init_prepare(
 #endif
 }
 
+#ifdef CONFIG_LEDS_MSM_GPIO_DUAL_REAR_FLASH_AND_FRONT_FLASH
+extern int msm_sensor_is_front_camera(void);
+#endif
+
 static int32_t msm_flash_low(
 	struct msm_flash_ctrl_t *flash_ctrl,
 	struct msm_flash_cfg_data_t *flash_data)
@@ -613,8 +617,24 @@ static int32_t msm_flash_low(
 					curr);
 			}
 			CDBG("low_flash_current[%d] = %d", i, curr);
+			#ifdef CONFIG_LEDS_MSM_GPIO_DUAL_REAR_FLASH_AND_FRONT_FLASH
+			if ((msm_sensor_is_front_camera() || flash_data->camera_id == 1))
+				led_trigger_event(flash_ctrl->torch_trigger[2],
+			                  curr);
+			else if (i < 2)
+				led_trigger_event(flash_ctrl->torch_trigger[i],
+			                  curr);
+			#elif defined(CONFIG_LEDS_MSM_GPIO_DUAL_FLASH)
+			if ((msm_sensor_is_front_camera()||flash_data->camera_id == 1))
+				led_trigger_event(flash_ctrl->torch_trigger[1],
+			                  curr);
+			else
+				led_trigger_event(flash_ctrl->torch_trigger[0],
+			                  curr);
+			#else
 			led_trigger_event(flash_ctrl->torch_trigger[i],
-				curr);
+			                  curr);
+			#endif
 		}
 	}
 	if (flash_ctrl->switch_trigger)
@@ -650,8 +670,24 @@ static int32_t msm_flash_high(
 					i, curr);
 			}
 			CDBG("high_flash_current[%d] = %d", i, curr);
+			#ifdef CONFIG_LEDS_MSM_GPIO_DUAL_REAR_FLASH_AND_FRONT_FLASH
+			if ((msm_sensor_is_front_camera() || flash_data->camera_id == 1))
+				led_trigger_event(flash_ctrl->flash_trigger[2],
+			                  curr);
+			else if(i < 2)
+				led_trigger_event(flash_ctrl->flash_trigger[i],
+			                  curr);
+			#elif defined(CONFIG_LEDS_MSM_GPIO_DUAL_FLASH)
+			if((msm_sensor_is_front_camera() || flash_data->camera_id == 1))
+				led_trigger_event(flash_ctrl->flash_trigger[1],
+			                  curr);
+			else
+				led_trigger_event(flash_ctrl->flash_trigger[0],
+			                  curr);
+			#else
 			led_trigger_event(flash_ctrl->flash_trigger[i],
-				curr);
+			                  curr);
+			#endif
 		}
 	}
 	if (flash_ctrl->switch_trigger)
@@ -1020,6 +1056,7 @@ static int32_t msm_flash_get_dt_data(struct device_node *of_node,
 		fctrl->flash_driver_type = FLASH_DRIVER_I2C;
 	}
 
+	#ifndef CONFIG_LEDS_MSM_GPIO_DUAL_REAR_FLASH_AND_FRONT_FLASH
 	/* Read the flash and torch source info from device tree node */
 	rc = msm_flash_get_pmic_source_info(of_node, fctrl);
 	if (rc < 0) {
@@ -1027,6 +1064,7 @@ static int32_t msm_flash_get_dt_data(struct device_node *of_node,
 			__func__, __LINE__, rc);
 		return rc;
 	}
+	#endif
 
 	/* Read the gpio information from device tree */
 	rc = msm_sensor_driver_get_gpio_data(
@@ -1039,11 +1077,22 @@ static int32_t msm_flash_get_dt_data(struct device_node *of_node,
 		return rc;
 	}
 
+	#ifndef CONFIG_PROJECT_GARLIC
 	if (fctrl->flash_driver_type == FLASH_DRIVER_DEFAULT)
 		fctrl->flash_driver_type = FLASH_DRIVER_GPIO;
+	#endif
 	CDBG("%s:%d fctrl->flash_driver_type = %d", __func__, __LINE__,
 		fctrl->flash_driver_type);
 
+	#ifdef CONFIG_LEDS_MSM_GPIO_DUAL_REAR_FLASH_AND_FRONT_FLASH
+	/* Read the flash and torch source info from device tree node */
+	rc = msm_flash_get_pmic_source_info(of_node, fctrl);
+	if (rc < 0) {
+		pr_err("%s:%d msm_flash_get_pmic_source_info failed rc %d\n",
+			__func__, __LINE__, rc);
+		return rc;
+	}
+	#endif
 	return rc;
 }
 
@@ -1069,6 +1118,20 @@ static long msm_flash_subdev_do_ioctl(
 	vdev = video_devdata(file);
 	sd = vdev_to_v4l2_subdev(vdev);
 	u32 = (struct msm_flash_cfg_data_t32 *)arg;
+
+	flash_data.cfg_type = u32->cfg_type;
+	#ifdef CONFIG_PLATFORM_TINNO
+	flash_data.camera_id = u32->camera_id;
+	#endif
+	for (i = 0; i < MAX_LED_TRIGGERS; i++) {
+		flash_data.flash_current[i] = u32->flash_current[i];
+		flash_data.flash_duration[i] = u32->flash_duration[i];
+	}
+
+	#ifdef CONFIG_LEDS_MSM_GPIO_DUAL_REAR_FLASH_AND_FRONT_FLASH
+	flash_data.flash_current[MAX_LED_TRIGGERS - 1] = flash_data.flash_current[MAX_LED_TRIGGERS - 2];
+	flash_data.flash_duration[MAX_LED_TRIGGERS - 1] = flash_data.flash_duration[MAX_LED_TRIGGERS - 2];
+	#endif
 
 	switch (cmd) {
 	case VIDIOC_MSM_FLASH_CFG32:
